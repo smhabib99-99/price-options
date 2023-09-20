664ccf3e3601684daf39b108992cdf65184156d8
smhabib99-99 <habibaust34@gmail.com>
2023-09-20 23:28:34 +0600

    price-options file created using react and tailwind css and daisy ui connected

---
+++ price-options/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-6xl bg-sky-600 mt-4 p-8'>price-options</h1>
+    </>
+  )
+}
+
+export default App
